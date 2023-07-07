4719c36a8d4d4686a3b70922a82f0b94fcc45227
BaoDev13 <baonv.uit@gmail.com>
2023-07-07 16:54:39 +0700

    export data

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,214 +1,274 @@
-import Table from 'react-bootstrap/Table';
-import { fetchAllUser } from '../services/UserServices';
-import { useEffect, useState } from 'react';
-import ReactPaginate from 'react-paginate';
-import ModalAddNew from './ModalAddNew';
-import ModalEditUsers from './ModalEditUsers';
-import ModalConfirm from './ModalConfirm';
-import _ from 'lodash';
-import { debounce } from 'lodash';
-import './TableUsers.scss';
+import Table from "react-bootstrap/Table";
+import { fetchAllUser } from "../services/UserServices";
+import { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
+import ModalAddNew from "./ModalAddNew";
+import ModalEditUsers from "./ModalEditUsers";
+import ModalConfirm from "./ModalConfirm";
+import _ from "lodash";
+import { debounce } from "lodash";
+import "./TableUsers.scss";
+import { CSVLink } from "react-csv";
 
 const TableUsers = (props) => {
+  const [listUsers, setListUsers] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
-    const [listUsers, setListUsers] = useState([]);
-    const [totalUsers, setTotalUsers] = useState(0);
-    const [totalPages, setTotalPages] = useState(0);
+  const [isShowModalAddNew, setIsShowModalAddNew] = useState(false);
 
-    const [isShowModalAddNew, setIsShowModalAddNew] = useState(false);
+  const [isShowModalEdit, setIsShowModalEdit] = useState(false);
+  const [dataUserEdit, setDataUserEdit] = useState({});
 
-    const [isShowModalEdit, setIsShowModalEdit] = useState(false);
-    const [dataUserEdit, setDataUserEdit] = useState({});
+  const [isShowModalDelete, setIsShowModalDelete] = useState(false);
+  const [dataUserDelete, setDataUserDelete] = useState({});
 
-    const [isShowModalDelete, setIsShowModalDelete] = useState(false);
-    const [dataUserDelete, setDataUserDelete] = useState({});
+  const [sortBy, setSortBy] = useState("asc");
+  const [sortField, setSortField] = useState("id");
 
-    const [sortBy, setSortBy] = useState("asc");
-    const [sortField, setSortField] = useState("id");
+  const [keyword, setKeyword] = useState("");
 
-    const [keyword, setKeyword] = useState("");
+  const [dataExport, setDataExport] = useState([]);
 
-    const handleClose = () => {
-        setIsShowModalAddNew(false);
-        setIsShowModalEdit(false);
-        setIsShowModalDelete(false);
+  const handleClose = () => {
+    setIsShowModalAddNew(false);
+    setIsShowModalEdit(false);
+    setIsShowModalDelete(false);
+  };
+
+  const handleUpdateTable = (user) => {
+    setListUsers([user, ...listUsers]);
+  };
+
+  const handleEditUserFromModal = (user) => {
+    let cloneListUsers = _.cloneDeep(listUsers);
+    let index = listUsers.findIndex((item) => item.id === user.id);
+    cloneListUsers[index].first_name = user.first_name;
+    setListUsers(cloneListUsers);
+  };
+
+  useEffect(() => {
+    //call api
+    getUser(1);
+  }, []);
+
+  const getUser = async (page) => {
+    let res = await fetchAllUser(page);
+    if (res && res.data) {
+      setTotalPages(res.total_pages);
+      setTotalUsers(res.total);
+      setListUsers(res.data);
     }
+  };
 
-    const handleUpdateTable = (user) => {
-        setListUsers([user, ...listUsers]);
+  const handlePageClick = (event) => {
+    // dau + de convert kieu string ve number khi minh chua biet bien selected co kieu gi
+    getUser(+event.selected + 1);
+  };
+
+  const handleEditUser = (user) => {
+    setDataUserEdit(user);
+    setIsShowModalEdit(true);
+  };
+
+  const handleDelete = (user) => {
+    setIsShowModalDelete(true);
+    setDataUserDelete(user);
+  };
+
+  const handleDeleteUserFromModal = (user) => {
+    let cloneListUsers = _.cloneDeep(listUsers);
+    cloneListUsers = cloneListUsers.filter((item) => item.id !== user.id);
+    setListUsers(cloneListUsers);
+  };
+
+  const handleSort = (sortBy, sortField) => {
+    setSortBy(sortBy);
+    setSortField(sortField);
+
+    let cloneListUsers = _.cloneDeep(listUsers);
+    //lodash sort
+    cloneListUsers = _.orderBy(cloneListUsers, [sortField], [sortBy]);
+    setListUsers(cloneListUsers);
+  };
+
+  const handleSearch = debounce((event) => {
+    let term = event.target.value;
+    if (term) {
+      let cloneListUsers = _.cloneDeep(listUsers);
+      cloneListUsers = cloneListUsers.filter((item) =>
+        item.email.includes(term)
+      );
+      setListUsers(cloneListUsers);
     }
-
-    const handleEditUserFromModal = (user) => {
-        let cloneListUsers = _.cloneDeep(listUsers);
-        let index = listUsers.findIndex(item => item.id === user.id);
-        cloneListUsers[index].first_name = user.first_name;
-        setListUsers(cloneListUsers);
+    //term empty thì lấy lại danh sách ban đầu
+    else {
+      getUser(1);
     }
+  }, 500);
 
-    
+  const getUserExport = (evnet, done) => {
+    let result = [];
+    if (listUsers && listUsers.length > 0) {
+      result.push(["Id", "Email", "First name", "Last name"]);
+      listUsers.map((item, index) => {
+        let arr = [];
+        arr[0] = item.id;
+        arr[1] = item.email;
+        arr[2] = item.first_name;
+        arr[3] = item.last_name;
+        result.push(arr);
+      });
 
-    useEffect(() => {
-        //call api
-        getUser(1);
-
-    }, [])
-
-    const getUser = async (page) => {
-        let res = await fetchAllUser(page);
-        if (res && res.data) {
-            setTotalPages(res.total_pages)
-            setTotalUsers(res.total)
-            setListUsers(res.data);
-        }
+      setDataExport(result);
+      done();
     }
+  };
 
-    const handlePageClick = (event) => {
-        // dau + de convert kieu string ve number khi minh chua biet bien selected co kieu gi
-        getUser(+event.selected + 1);
-    }
+  return (
+    <>
+      <div className="my-3 add-new">
+        <span>
+          <b>List Users:</b>
+        </span>
+        <div className="group-btns">
+          <label className="btn btn-warning" htmlFor="import">
+            <i className="fa-solid fa-file-import"></i> Import
+          </label>
+          <input id="import" type="file" hidden />
 
-    const handleEditUser = (user) => {
-        setDataUserEdit(user);
-        setIsShowModalEdit(true);
-    }
+          <CSVLink
+            data={dataExport}
+            filename={"users.csv"}
+            className="btn btn-primary"
+            asyncOnClick={true}
+            onClick={getUserExport}
+          >
+            <i className="fa-solid fa-file-import"></i> Export
+          </CSVLink>
 
-    const handleDelete = (user) => {
-        setIsShowModalDelete(true);
-        setDataUserDelete(user);
-    }
-
-    const handleDeleteUserFromModal = (user) => {
-        let cloneListUsers = _.cloneDeep(listUsers);
-        cloneListUsers = cloneListUsers.filter(item => item.id !== user.id);
-        setListUsers(cloneListUsers);
-    }
-
-    const handleSort = (sortBy, sortField) => {
-        setSortBy(sortBy);
-        setSortField(sortField);
-
-        let cloneListUsers = _.cloneDeep(listUsers);
-        //lodash sort
-        cloneListUsers = _.orderBy(cloneListUsers, [sortField], [sortBy]);
-        setListUsers(cloneListUsers);
-    }
-
-    const handleSearch = debounce((event) => {
-        let term = event.target.value
-        if(term){
-            let cloneListUsers = _.cloneDeep(listUsers);
-            cloneListUsers = cloneListUsers.filter(item => item.email.includes(term));
-            setListUsers(cloneListUsers);
-        }else{
-            getUser(1);
-        }
-    }, 500)
-
-    return (<>
-        <div className='my-3 add-new'>
-            <span>List Users:</span>
-            <button className='btn btn-success'
-                onClick={() => setIsShowModalAddNew(true)}>Add new user</button>
+          <button
+            className="btn btn-success button-addnew"
+            onClick={() => setIsShowModalAddNew(true)}
+          >
+            <i className="fa-solid fa-circle-plus"></i> Add new
+          </button>
         </div>
+      </div>
 
-        <div className='col-4 my-3'>
-            <input 
-            className='form-control' 
-            placeholder='Search user by email...'
-            // value={keyword}
-            onChange={(event) => handleSearch(event)}
-            />
-        </div>
+      <div className="col-4 my-3">
+        <input
+          className="form-control"
+          placeholder="Search user by email..."
+          // value={keyword}
+          onChange={(event) => handleSearch(event)}
+        />
+      </div>
 
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>
-                        <div className='sort-header'>
-                            <span>ID</span>
-                            <span>
-                                <i className='fa-solid fa-arrow-down-long'
-                                   onClick={() => handleSort("desc","id")}
-                                ></i>
-                                <i className='fa-solid fa-arrow-up-long'
-                                   onClick={() => handleSort("asc","id")}
-                                ></i>
-                            </span>
-                        </div>
-                    </th>
-                    <th >Email</th>
-                    <th>
-                        <div className='sort-header'>
-                            <span>First Name</span>
-                            <span>ID</span>
-                            <span>
-                                <i className='fa-solid fa-arrow-down-long'
-                                   onClick={() => handleSort("desc","first_name")}
-                                ></i>
-                                <i className='fa-solid fa-arrow-up-long'
-                                   onClick={() => handleSort("asc","first_name")}
-                                ></i>
-                            </span>
-                        </div>
-                    </th>
-                    <th>Last Name</th>
-                    <th>Actions</th>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>
+              <div className="sort-header">
+                <span>ID</span>
+                <span>
+                  <i
+                    className="fa-solid fa-arrow-down-long"
+                    onClick={() => handleSort("desc", "id")}
+                  ></i>
+                  <i
+                    className="fa-solid fa-arrow-up-long"
+                    onClick={() => handleSort("asc", "id")}
+                  ></i>
+                </span>
+              </div>
+            </th>
+            <th>Email</th>
+            <th>
+              <div className="sort-header">
+                <span>First Name</span>
+                <span>ID</span>
+                <span>
+                  <i
+                    className="fa-solid fa-arrow-down-long"
+                    onClick={() => handleSort("desc", "first_name")}
+                  ></i>
+                  <i
+                    className="fa-solid fa-arrow-up-long"
+                    onClick={() => handleSort("asc", "first_name")}
+                  ></i>
+                </span>
+              </div>
+            </th>
+            <th>Last Name</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUsers &&
+            listUsers.length > 0 &&
+            listUsers.map((item, index) => {
+              return (
+                <tr key={`users-${index}`}>
+                  <td>{item.id}</td>
+                  <td>{item.email}</td>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                  <td>
+                    <button
+                      className="btn btn-warning mx-3"
+                      onClick={() => handleEditUser(item)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => handleDelete(item)}
+                    >
+                      Delete
+                    </button>
+                  </td>
                 </tr>
-            </thead>
-            <tbody>
-                {listUsers && listUsers.length > 0 &&
-                    listUsers.map((item, index) => {
-                        return (
-                            <tr key={`users-${index}`}>
-                                <td>{item.id}</td>
-                                <td>{item.email}</td>
-                                <td>{item.first_name}</td>
-                                <td>{item.last_name}</td>
-                                <td>
-                                    <button className='btn btn-warning mx-3' onClick={() => handleEditUser(item)}>Edit</button>
-                                    <button className='btn btn-danger' onClick={() => handleDelete(item)}>Delete</button>
-                                </td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-        </Table>
-        <ReactPaginate
-            breakLabel="..."
-            nextLabel="next >"
-            onPageChange={handlePageClick}
-            pageRangeDisplayed={2}
-            pageCount={totalPages}
-            previousLabel="< previous"
-            pageClassName="page-item"
-            pageLinkClassName="page-link"
-            previousClassName="page-item"
-            previousLinkClassName="page-link"
-            nextClassName="page-item"
-            nextLinkClassName="page-link"
-            breakClassName="page-item"
-            breakLinkClassName="page-link"
-            containerClassName="pagination"
-            activeClassName="active"
-        />
-        <ModalAddNew
-            show={isShowModalAddNew}
-            handleClose={handleClose}
-            handleUpdateTable={handleUpdateTable}
-        />
-        <ModalEditUsers
-            show={isShowModalEdit}
-            dataUserEdit={dataUserEdit}
-            handleClose={handleClose}
-            handleEditUserFromModal={handleEditUserFromModal}
-        />
-        <ModalConfirm
-            show={isShowModalDelete}
-            handleClose={handleClose}
-            dataUserDelete={dataUserDelete}
-            handleDeleteUserFromModal={handleDeleteUserFromModal}
-        />
-    </>)
-}
+              );
+            })}
+        </tbody>
+      </Table>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={2}
+        pageCount={totalPages}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+      />
+      <ModalAddNew
+        show={isShowModalAddNew}
+        handleClose={handleClose}
+        handleUpdateTable={handleUpdateTable}
+      />
+      <ModalEditUsers
+        show={isShowModalEdit}
+        dataUserEdit={dataUserEdit}
+        handleClose={handleClose}
+        handleEditUserFromModal={handleEditUserFromModal}
+      />
+      <ModalConfirm
+        show={isShowModalDelete}
+        handleClose={handleClose}
+        dataUserDelete={dataUserDelete}
+        handleDeleteUserFromModal={handleDeleteUserFromModal}
+      />
+    </>
+  );
+};
 export default TableUsers;
